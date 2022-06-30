7aa572d606b7b5092ccf894163b944b800a5dd5a
Gábor Hajdu <gabor.hajdu@codecool.com>
2022-06-30 15:35:00 +0200

    fix interval handling

--- src/components/Timer2.js
+++ src/components/Timer2.js
@@ -1,30 +1,21 @@
 // rfc ->
 
-import React, { useEffect, useState } from 'react' // useState az egy 'hook', ezeket hasznaljuk a reactben | mindig legkulso szintjen hivhato meg.
+import React, { useEffect, useState } from 'react' // useState az egy 'hook', ezeket hasznaljuk a reactben | mindig a komponens legkulso szintjen hivhato meg.
 
 export default function Timer2() {
-    function incrementTimer() {
-        setTime(
-            time+1
-        )
-    };
-
     const [time, setTime] = useState(0);
 
     useEffect(() => {
         console.log('new interval set')
 
         const intervalId = setInterval(() => {
-
-            incrementTimer()
+            setTime(t => t+1)
         }, 1000)
 
         return () => {
-
             clearInterval(intervalId)
         }
- 
-    });
+    }, []);
     
 
     return (
@@ -32,7 +23,7 @@ export default function Timer2() {
             <div>
                 {time}
             </div>
-            <button onClick={ incrementTimer }>click me</button>
+            <button onClick={ () => setTime(time+1) }>click me</button>
             {/* <button onClick={() => { incrementTimer() }}>click me</button> */}
         </>
     );
