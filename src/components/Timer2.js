e9ef2333dcf94b6f65a87e27a41d33b754f6d17f
gremgergo <gremgergo@gmail.com>
2022-06-30 11:38:02 +0200

    country react

--- src/components/Timer2.js
+++ src/components/Timer2.js
@@ -1,39 +1,49 @@
 // rfc ->
 
-import React, { useEffect, useState } from 'react' // useState az egy 'hook', ezeket hasznaljuk a reactben | mindig legkulso szintjen hivhato meg.
+import React, { useEffect, useState } from "react"; // useState az egy 'hook', ezeket hasznaljuk a reactben | mindig legkulso szintjen hivhato meg.
 
 export default function Timer2() {
-    function incrementTimer() {
-        setTime(
-            time+1
-        )
-    };
+  const [time2, setTime] = useState(0);
+  const [countryName, setCountryName] = useState("");
+  const [country, setCountry] = useState(null);
 
-    const [time, setTime] = useState(0);
+  useEffect(() => { // valtozas esemenykezeloje
+    console.log("time changed");
+  }, [time2]);
 
-    useEffect(() => {
-        console.log('new interval set')
+  useEffect(() => { 
+    async function fetchData() {
+      // You can await here
+      let response = await fetch(
+        `https://restcountries.com/v3.1/name/${countryName}`
+      );
+      const data = await response.json();
 
-        const intervalId = setInterval(() => {
+      setCountry(data[0].name.nativeName[data[0].cca3.toLowerCase()].common);
+    }
+    if (countryName !== "") {
+        fetchData();
+    }
+  }, [countryName]);
 
-            incrementTimer()
-        }, 1000)
-
-        return () => {
-
-            clearInterval(intervalId)
-        }
- 
-    });
-    
-
-    return (
-        <> 
-            <div>
-                {time}
-            </div>
-            <button onClick={ incrementTimer }>click me</button>
-            {/* <button onClick={() => { incrementTimer() }}>click me</button> */}
-        </>
-    );
+  return (
+    <>
+      <div>{time2}</div>
+      <input
+        type="text"
+        onChange={(event) => {
+          setCountryName(event.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          setTime(time2 + 1);
+        }}
+      >
+        click me
+      </button>
+      {/* <button onClick={() => { incrementTimer() }}>click me</button> */}
+      <div>{country}</div>
+    </>
+  );
 }
